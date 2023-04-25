f7ce2d9c977985bfe0838bed7f046b903a56defe
nguyen2109 <72661673+nguyen2109@users.noreply.github.com>
2023-04-26 03:45:03 +0700

    update file

--- 10.php
+++ 10.php
@@ -1,5 +1,31 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
+<?php
+class Dog 
+{
+	var $mau_mat = "Đen";
+	var $mau_long = "Xám";
+	var $tai;
+	
+	function Nose()
+	{
+		# code...
+		echo "Nose Dog";
+	}
+	function DoiMauMat(){
+		$this->mau_mat = "Xanh";
+
+	}
+	function DoiMauLong(){
+		$this->mau_long = "Tím";
+	}
+	function TaoKieuTai(){
+		$this->tai = "Tai cụp";
+	}
+	
+}
+
+?>
 <section class="content">
 
 	<aside class="col-xs-4">
@@ -12,7 +38,28 @@
 <article class="main-content col-xs-8">
 
 	
-	<?php  
+	<?php
+	$husky = new Dog();
+	echo "Màu mắt: ".$husky->mau_mat."<br>";
+	echo "Màu lông: ".$husky->mau_long."<br>";
+	// thực hiện đổi màu mắt: 
+	$husky->DoiMauMat();
+	echo "Màu mắt: ".$husky->mau_mat."<br>";
+	//thực hiện đổi màu lông cho husky nhé : 
+	$husky->DoiMauLong();
+	echo "Màu lông: ".$husky->mau_long."<br>";
+	// thực hiển tạo kiểu tai:
+	$husky->TaoKieuTai();
+	echo "Kiểu tai: ".$husky->tai."<br>";
+
+
+	  if (method_exists("Dog","Nose")) {
+		# code... kiểm tra xem class Dog có tồn tại hay không?
+		echo "ok<br>";
+	} else {
+		
+		echo "error";
+	}
 
 	/*  Step 1: Use the Make a class called Dog
 
